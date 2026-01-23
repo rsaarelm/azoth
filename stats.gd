2f61cec72a4c7b91edc2e059fb3eb90c2c7fd273
Risto Saarelma <risto.saarelma@iki.fi>
2026-01-23 19:26:39 +0200

    Rename stats

--- stats.gd
+++ stats.gd
@@ -3,15 +3,15 @@ class_name Stats extends RefCounted
 ## Primary and derived stats for player characters.
 
 @export_storage var might := 0
-@export_storage var trickery := 0
 @export_storage var faith := 0
-@export_storage var deftness := 0
+@export_storage var grace := 0
+@export_storage var cunning := 0
 
 var level:
 	get:
-		return might + trickery + faith + deftness
+		return might + faith + grace + cunning
 
 var health:
 	get:
 		# Health is derived from the base stats.
-		return 30 + 5 * might + 4 * faith + 4 * deftness + 3 * trickery
+		return 30 + 5 * might + 4 * faith + 4 * grace + 3 * cunning
